81c2e1ff7c5aac5632b368aa7b209d45b78f5a3e
matteo9723 <matteolandri7@gmail.com>
2021-11-23 21:18:58 +0100

    bonus

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <Title/>
+    <Jumbotron/>
   </div>
 </template>
 
 <script>
 
+  import Jumbotron from './components/Jumbotron.vue'
   import Title from'./components/Title.vue'
+
 export default {
   name: 'App',
   components: {
-    Title
+    Title,
+    Jumbotron
   }
 }
 </script>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    jumbotron
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style scoped> 
+ /* quando inserisco scoped devo aggiornare la pagina se no non visualizzo il cambiamento */
+  div{
+    color: red;
+    background-color: aqua;
+    text-align: center;
+  }
+</style>
